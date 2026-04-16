ff96aff5c9399eb70d2093cdfdc550aae5dabf9c
Said Osmanov <saidelleausmaneuff@gmail.com>
2026-04-16 17:15:39 +0300

    elt(extract): add header with auth token

--- elt/extract.py
+++ elt/extract.py
@@ -1,5 +1,7 @@
 import requests
 from tenacity import retry, stop_after_attempt, wait_exponential_jitter
+import json
+import tempfile
 from typing import Any
 from logger import get_logger
 
@@ -15,12 +17,14 @@ def fetch_api_data(
     session: requests.Session,
     url: str,
     params: dict[str, Any],
+    headers: dict[str, str],
     timeout: int = 30
 ) -> dict[str, Any]:
     try:
         logger.info(f"Fetching job postings data from {url}...")
-        response = session.get(url=url, params=params, timeout=timeout)
+        response = session.get(url=url, params=params, headers=headers, timeout=timeout)
         response.raise_for_status()
+        logger.info("Page of job postings data successfully fetched.")
         return response.json()
     except requests.exceptions.HTTPError as exc:
         logger.error(f"Server responded with {exc}", exc_info=exc)
