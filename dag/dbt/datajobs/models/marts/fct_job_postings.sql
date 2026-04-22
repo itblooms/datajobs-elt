e980090579a29d01a225bfc18a6ebdf04193673a
Said Osmanov <saidelleausmaneuff@gmail.com>
2026-04-22 23:40:21 +0300

    feat(star sch): add fact table

---
+++ dag/dbt/datajobs/models/marts/fct_job_postings.sql
@@ -0,0 +1,26 @@
+select
+    id,
+    job_title,
+    seniority_level,
+    salary_from,
+    salary_to,
+    currency,
+    pay_type,
+    pay_period,
+    experience,
+    requirenments,
+    responsibilities,
+    schedule,
+    is_internship,
+    completness
+    created_at,
+    created_at_weekday,
+    e.id as employer_id,
+    l.id as location_id
+from {{ ref('int_job_postings__enriched') }} jp
+left join {{ ref('dim_employers') }} e on jp.employer_id = e.id
+left join {{ ref('dim_locations') }} l
+    on jp.country_id = l.country_id
+    and jp.city_name = l.city_name
+    and jp.street_name = l.street_name
+    and jp.building = l.building
